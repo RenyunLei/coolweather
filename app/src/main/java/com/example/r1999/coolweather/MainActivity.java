1d8d2c14194f2a72f97103458549804a546bcda0
RenyunLei <2199799547@qq.com>
2018-01-08 14:40:55 +0800

    First com.

---
+++ app/src/main/java/com/example/r1999/coolweather/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.r1999.coolweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
